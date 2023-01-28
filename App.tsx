03c4dfe8ae0af202a38dc42909971267b16e302a
JalpeshAhir <jalpeshahir6512@gmail.com>
2023-01-28 17:53:51 +0530

    frist commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
+import React from "react";
+import { View, Text } from 'react-native'
+
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        hii
+      </Text>
+    </View>
+  )
+}
+
+export default App
